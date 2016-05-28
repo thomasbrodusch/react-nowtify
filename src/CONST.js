beb085856f58e4269f4964b9811ef59f5ae3f737
Thomas B <tom4dev@users.noreply.github.com>
2016-05-28 15:48:20 +0200

    add separated file who contains all const var

---
+++ src/CONST.js
@@ -0,0 +1,10 @@
+const defaultConfig = {
+  containerID: 'nowtify-wrapper',
+  displayTimeout: 5000,
+  transition: 'growl',
+  sound: false,
+  dismissible: false,
+  keepHistory: false,
+};
+
+export { defaultConfig };
